b4d658d5314a5235049ae5cef922de9f7ced9138
FinnWiel <finnvandewiel01@gmail.com>
2026-08-20 21:37:13 +0200

    Apply Falqo brand color and refine logo size

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -4,14 +4,14 @@
 
 @if($sidebar)
     <flux:sidebar.brand :name="config('app.name', 'Laravel')" {{ $attributes }}>
-        <x-slot name="logo" class="flex aspect-square size-9 items-center justify-center">
-            <x-app-logo-icon class="size-9 object-contain" />
+        <x-slot name="logo" class="flex aspect-square size-7 items-center justify-center">
+            <x-app-logo-icon class="size-7 rounded-md object-contain" />
         </x-slot>
     </flux:sidebar.brand>
 @else
     <flux:brand :name="config('app.name', 'Laravel')" {{ $attributes }}>
-        <x-slot name="logo" class="flex aspect-square size-9 items-center justify-center">
-            <x-app-logo-icon class="size-9 object-contain" />
+        <x-slot name="logo" class="flex aspect-square size-7 items-center justify-center">
+            <x-app-logo-icon class="size-7 rounded-md object-contain" />
         </x-slot>
     </flux:brand>
 @endif
