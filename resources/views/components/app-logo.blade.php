3626873379177f493bd2bbb2e738191d41d07774
FinnWiel <finnvandewiel01@gmail.com>
2026-08-21 18:02:44 +0200

    align Falqo app with cobalt brand system

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -3,15 +3,15 @@
 ])
 
 @if($sidebar)
-    <flux:sidebar.brand :name="config('app.name', 'Laravel')" {{ $attributes }}>
-        <x-slot name="logo" class="flex aspect-square size-7 items-center justify-center">
-            <x-app-logo-icon class="size-7 rounded-md object-contain" />
+    <flux:sidebar.brand :name="config('app.name', 'Laravel')" {{ $attributes->class('brand-wordmark') }}>
+        <x-slot name="logo" class="flex aspect-square !size-[34px] items-center justify-center !rounded-[10px]">
+            <x-app-logo-icon class="size-[34px] object-contain" />
         </x-slot>
     </flux:sidebar.brand>
 @else
-    <flux:brand :name="config('app.name', 'Laravel')" {{ $attributes }}>
-        <x-slot name="logo" class="flex aspect-square size-7 items-center justify-center">
-            <x-app-logo-icon class="size-7 rounded-md object-contain" />
+    <flux:brand :name="config('app.name', 'Laravel')" {{ $attributes->class('brand-wordmark') }}>
+        <x-slot name="logo" class="flex aspect-square !size-[34px] items-center justify-center !rounded-[10px]">
+            <x-app-logo-icon class="size-[34px] object-contain" />
         </x-slot>
     </flux:brand>
 @endif
