7a0792732faefae03b9e6ee1df8b3a5ced69e9e3
FinnWiel <finnvandewiel01@gmail.com>
2026-08-22 19:01:03 +0200

    Improve interactive cursor and appearance controls

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -24,11 +24,11 @@
 
             <flux:spacer />
 
-            <div class="mb-3 flex justify-center px-2">
-                <flux:radio.group x-data variant="segmented" x-model="$flux.appearance" size="sm" aria-label="{{ __('Appearance') }}">
-                    <flux:radio value="light" icon="sun" aria-label="{{ __('Light') }}" />
-                    <flux:radio value="dark" icon="moon" aria-label="{{ __('Dark') }}" />
-                    <flux:radio value="system" icon="computer-desktop" aria-label="{{ __('Auto') }}" />
+            <div class="mb-3 w-full px-2">
+                <flux:radio.group x-data variant="segmented" x-model="$flux.appearance" size="sm" class="w-full" aria-label="{{ __('Appearance') }}">
+                    <flux:radio value="light" icon="sun" class="flex-1" aria-label="{{ __('Light') }}" />
+                    <flux:radio value="dark" icon="moon" class="flex-1" aria-label="{{ __('Dark') }}" />
+                    <flux:radio value="system" icon="computer-desktop" class="flex-1" aria-label="{{ __('Auto') }}" />
                 </flux:radio.group>
             </div>
 
