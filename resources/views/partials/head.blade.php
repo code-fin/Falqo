7681093029acd66009ac0d306720865686e00311
FinnWiel <finnvandewiel01@gmail.com>
2026-08-22 21:46:37 +0200

    Adapt favicon contrast to browser theme

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -7,7 +7,9 @@
     {{ filled($title ?? null) ? $title.' - '.config('app.name', 'Laravel') : config('app.name', 'Laravel') }}
 </title>
 
-<link rel="icon" href="/favicon-falcon-transparent.png?v=2" type="image/png">
+<link rel="icon" href="/favicon-falcon-black.png?v=3" type="image/png">
+<link rel="icon" href="/favicon-falcon-black.png?v=3" type="image/png" media="(prefers-color-scheme: light)">
+<link rel="icon" href="/favicon-falcon-transparent.png?v=3" type="image/png" media="(prefers-color-scheme: dark)">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
 @fonts
